4b97f0b558615a650d0d8e258f70a4d183c5be73
geraldpro <frankgerald88@gmail.com>
2016-09-07 09:07:49 +0100

    first commit

---
+++ CircleAreaCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleAreaCalculator
+{
+    class circle
+    {
+        int radius1; // Member Varialble
+
+        public int acceptparameters()
+        {
+            Console.Write("Enter radius value and press enter to run : \t");
+            radius1 = Convert.ToInt32(Console.ReadLine()); 
+            return radius1;
+        }
+       
+        public double Area(int radius)
+        {
+            double area;
+            area = Math.PI * radius * radius;
+            return area;
+        }
+
+        static void Main(string[] args)
+        {
+            int radius;
+            double area;
+            circle c = new circle();
+            radius = c.acceptparameters();
+            area = c.Area(radius);
+            Console.WriteLine(area);
+            Console.ReadLine();
+
+        }        
+    }
+
+        
+}
